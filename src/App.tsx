49ee3a9cecbe6cca4ca81804d6283689cc96ed54
Walisson Mariano <marianowalisson98@gmail.com>
2024-10-16 07:16:13 -0300

    feat: insert eslint in project.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React</h1> 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
